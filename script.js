c878188a02979bded37673fba5c9e4f3dd4d653b
Ishika Verma <ishikaverma2317@gmail.com>
2025-11-05 10:49:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-// complete the given function
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
 
-function palindrome(str){
+let input = "";
 
+process.stdin.on("data", function (chunk) {
+  input += chunk;
+});
+
+process.stdin.on("end", function () {
+  input = input.trim();
+  console.log(isPalindrome(input));
+});
+
+function isPalindrome(s) {
+  // Remove non-alphanumeric chars and convert to lowercase
+  const cleanStr = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+  // Compare the cleaned string with its reverse
+  return cleanStr === cleanStr.split("").reverse().join("");
 }
-module.exports = palindrome
